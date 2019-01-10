1a3dcebfc33f8282d03ce7ee2795a84b4c8b331f
Daniel <daniel@ubuntu.local>
2019-01-10 13:59:16 +0100

    The text is now neatly padded. 2 levels are included in the parser.

--- scripts/npm/generate_docs.js
+++ scripts/npm/generate_docs.js
@@ -38,32 +38,108 @@ let output = '';
 function levelTabs(numLevels) {
     let output = '';
 
-    for(let i = 0; i < numLevels; i++) {
+    if (!isNumber(numLevels) || numLevels < 0)
+        return output;
+
+    for (let i = 0; i < numLevels; i++) {
         output += '\t';
     }
 
     return output;
 }
 
+function arrayToPathString(path) {
+    let output = '';
+
+    if (Array.isArray(path)) {
+        path.forEach(function (item) {
+                output += item + '/';
+            }
+        );
+    }
+
+    return output;
+}
+
+function isNumber(param) {
+    return 'number' === typeof(param);
+}
+
+function repeatSpaces(num) {
+    if(isNumber(num) && num > 0) {
+        let output = '';
+
+        for(let i = 0; i < num; i++) {
+            output += ' ';
+        }
+
+        return output;
+    }
+
+    return '';
+}
+
+function padStringToCharacters(stringToMeasure, padding) {
+    if(isString(stringToMeasure)) {
+        return repeatSpaces(padding - stringToMeasure.length);
+    }
+
+    return '';
+}
+
 function addItem(path, description, level) {
-    output += levelTabs(level) + 'GET /' + path + '\t\t' + description + EOL;
+    const part1 = levelTabs(level) + 'GET /' + arrayToPathString(path);
+
+    output += part1;
+    output += padStringToCharacters(part1, 50) + description + EOL;
 }
 
 // start with the general help message
 output += inputJson['_help'] + EOL + EOL;
 
-for (const outerKey in inputJson) {
+// now browse through the top level keys
+function isObject(param) {
+    return 'object' === typeof (param);
+}
+
+function isString(param) {
+    return 'string' === typeof (param);
+}
+
+// level: 0
+for (const level0Key in inputJson) {
     // look for subkey called "_this"
     // these are valid operations
-    if (inputJson.hasOwnProperty(outerKey)) {
-        const subkey = inputJson[outerKey];
+    if (inputJson.hasOwnProperty(level0Key)) {
+        const level1Key = inputJson[level0Key];
 
-        if ('object' === typeof subkey && subkey.hasOwnProperty('_this')) {
-            addItem(outerKey, subkey['_this'], 0);
+        if (isObject(level1Key) && level1Key.hasOwnProperty('_this')) {
+            addItem([level0Key], level1Key['_this'], 0);
+
+            // level: 1
+            // go through the entries underneath the '_ref' object
+            const refObject = level1Key['_ref'];
+
+            if (isObject(refObject)) {
+                // level: 2
+                for (const level2Key in refObject) {
+                    if (isString(level2Key) && !level2Key.startsWith('_')) {
+                        const level2Object = refObject[level2Key];
+
+                        if(isObject(level2Object)) {
+                            // internal levels do not correspond 1-1 to the generated levels
+                            let level2ObjectDescription = level2Object['_this'] || level2Object['_ref']['_this'];
+
+                            addItem([level0Key, level2Key], level2ObjectDescription, 1);
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
+// write the docs to a file
 stream.once('open', function (fd) {
     stream.write(output);
     stream.end();
