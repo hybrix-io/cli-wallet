f9ea75ca9fb699c5114beb9a2dd74d1b8b0ad353
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2019-02-12 15:34:16 +0100

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'plugins': ['standard'],
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'semistandard',
+  'parserOptions': {
+    'sourceType': 'module'
+  },
+  'rules': {
+    'no-undef': 1,
+    'no-global-assign': 1,
+    'no-unused-vars': 1,
+    'camelcase': 1,
+    'no-var': 1,
+    'object-curly-spacing': 0
+  }
+};
