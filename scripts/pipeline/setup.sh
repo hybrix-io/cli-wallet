6862696974bfd4739391f994828315d00927d59a
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2020-07-14 14:17:10 +0200

    Update setup.sh

--- scripts/pipeline/setup.sh
+++ scripts/pipeline/setup.sh
@@ -9,7 +9,7 @@ echo "[.] Install jsdoc"
 npm install -g jsdoc
 
 echo "[.] Get common"
-curl --location --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://gitlab.com/api/v4/projects/hybrix%2Fhybrixd%2Fcommon/jobs/artifacts/master/download?job=common" -o artifacts-common.zip
+curl --location --header "PRIVATE-TOKEN:$HYBRIX_BOT_GITLAB_PIPELINE_TOKEN" "https://gitlab.com/api/v4/projects/hybrix%2Fhybrixd%2Fcommon/jobs/artifacts/master/download?job=common" -o artifacts-common.zip
 
 # remove link to common and unzip the downloaded artifact to the directory (|| true --> on error, no problem)
 rm -rf  common || true
@@ -19,7 +19,7 @@ unzip -q -o artifacts-common.zip -d common
 rm -rf  artifacts-common.zip || true
 
 echo "[.] Get interface"
-curl --location --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://gitlab.com/api/v4/projects/hybrix%2Fhybrixd%2Finterface/jobs/artifacts/master/download?job=interface" -o artifacts-interface.zip
+curl --location --header "PRIVATE-TOKEN:$HYBRIX_BOT_GITLAB_PIPELINE_TOKEN" "https://gitlab.com/api/v4/projects/hybrix%2Fhybrixd%2Finterface/jobs/artifacts/master/download?job=interface" -o artifacts-interface.zip
 
 # remove link to interface and unzip the downloaded artifact to the directory (|| true --> on error, no problem)
 rm -rf  interface || true
