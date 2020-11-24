d41cc9c137f26e055a1b98d3dd60b828f19c833d
Agent725 <agent725@725.be>
2020-11-24 15:59:29 +0100

    accept deal, returning transaction ID

--- modules/deal/accept.js
+++ modules/deal/accept.js
@@ -7,11 +7,21 @@ exports.description = 'Accept a deal and send funds [argument: dealID]';
 
 exports.accept = (ops) => (dealID) => [
   getLogin(ops, {...this, host: ''}), 'session',
-  {query: '/e/deal/status/' + dealID}, 'rout'
-  /*
-  obj => ({
-    accountID: {data: obj.accountID, step: 'id'},
-    txid: [{symbol:obj.deal.ask.symbol, amount:obj.deal.ask.amount, target:obj.deal.ask.target}, 'transaction'],
+  keys => ({
+    id: [{data: 'account ' + keys.secretKey}, 'hash', hash => ({data: hash, source: 'hex', target: 'base58'}), 'code'],
+    deal: [{query: '/e/deal/status/' + dealID}, 'rout']
   }), 'parallel',
-  obj => { return {query: '/e/deal/claim/' + obj.accountID + '/' + obj.txid}; }, 'rout' */
+  obj => ( (obj.deal.status==='open' && obj.deal.progress==0)
+    ? [
+        {symbol:obj.deal.ask.symbol, amount:obj.deal.ask.amount, target:obj.deal.ask.target}, 'transaction',
+        txid => (
+          {
+            claim: [{query: '/e/deal/claim/' + dealID + '/' + txid},'rout'],
+            txid: {data: result.txid, step: 'id'}
+          }
+        ), 'parallel',
+        result => (result.txid)
+      ]
+    : [{data:'Deal ' + dealID + ' already has been accepted.'},'string']
+  ), 'sequential'
 ];
