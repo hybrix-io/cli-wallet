ef07c632abaa3f20243055485b4ef7b2556ca119
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2020-12-10 16:34:07 +0100

    Add EULA constent mechanism

---
+++ modules/allocation/EULA.md
@@ -0,0 +1,8 @@
+All your funds are belong to us.
+
+This is a test
+
+Do you agree to give to us
+- all your moneys
+- all your candy
+- your firstborn child
--- modules/allocation/create.js
+++ modules/allocation/create.js
@@ -1,9 +1,33 @@
 const {getLogin} = require('../../lib/setup');
+const fs = require('fs');
+const readlineSync = require('readline-sync');
 
 exports.host = 'allocation';
 exports.description = 'Create new allocation account, returning the ID';
 
+const checkEULA = ops => () => {
+  let answer;
+  if (fs.existsSync('./modules/allocation/.EULA.consent')) {
+    console.log('[i] EULA has been previously agreed to.');
+  } else if (ops.yes) {
+    answer = 'y';
+    if (ops.verbose) console.log('[i] Automatically consented to EULA.');
+  } else {
+    console.log(fs.readFileSync('./modules/allocation/EULA.md').toString());
+    while (answer !== 'y' && answer !== 'n') {
+      answer = readlineSync.question('Do you agree [y/n]: ');
+    }
+  }
+  if (answer === 'y') { // write consent to file
+    if (ops.verbose) console.log('[i] Consented to EULA.');
+    fs.writeFileSync('./modules/allocation/.EULA.consent', (new Date()).toString());
+  }
+  return {condition: answer === 'y', message: 'Could not continue without EULA approval.'};
+};
+
 exports.create = (ops) => () => [
+  checkEULA(ops), 'assert',
+
   getLogin(ops, this), 'getLoginKeyPair',
   keys => ({
     secretKey: {data: keys.secretKey, step: 'id'},
