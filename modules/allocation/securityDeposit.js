e1851c7ff37e522d1e2e35531ef1845b8c705692
Agent725 <agent725@725.be>
2021-03-04 13:23:40 +0100

    change security deposit default help message to HY

--- modules/allocation/securityDeposit.js
+++ modules/allocation/securityDeposit.js
@@ -2,7 +2,7 @@ const {getSignatureSteps} = require('./pairDelete');
 
 exports.args = 1;
 exports.host = 'allocation';
-exports.description = 'Sequestrate an amount of TOMO.HY from your allocation account to the security reserve [argument: amount]';
+exports.description = 'Sequestrate an amount of HY from your allocation account to the security reserve [argument: amount]';
 
 exports.securityDeposit = (ops) => (amount) => [
   ...getSignatureSteps(ops, this, 'securityReserveAccount', ['deposit', amount]),
--- modules/allocation/securityWithdraw.js
+++ modules/allocation/securityWithdraw.js
@@ -2,7 +2,7 @@ const {getSignatureSteps} = require('./pairDelete');
 
 exports.args = 1;
 exports.host = 'allocation';
-exports.description = 'Extract an amount of TOMO.HY from the security reserve to your allocation account [argument: amount]';
+exports.description = 'Extract an amount of HY from the security reserve to your allocation account [argument: amount]';
 
 exports.securityWithdraw = (ops) => (amount) => [
   ...getSignatureSteps(ops, this, 'securityReserveAccount', ['withdraw', amount]),
