efc54642cbd204a66a3314fa2ecb47eab803705f
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-06-08 21:06:42 +0200

    Reduce code

--- modules/allocation/pairStats.js
+++ modules/allocation/pairStats.js
@@ -4,20 +4,13 @@ exports.host = 'allocation';
 exports.description = 'Show pair stats (sorted by volume)';
 exports.requireLogin = false;
 
-function sortObj(obj) {
-  var array = [];
-  for (var key in obj) {
-    array.push({pair:key,ask:obj[key].ask,bid:obj[key].bid,volume:obj[key].volume,fee:obj[key].fee,sufficiency:obj[key].sufficiency,liquidity:obj[key].liquidity,updated:obj[key].updated});
-  }
-  array.sort(function(a, b) {
-      return a.volume - b.volume;
-  });
-  return array;
+function sortPairsByVolume (pairs) {
+  return Object.entries(pairs)
+    .map(([pair, pairData]) => ({pair, ...pairData}))
+    .sort((a, b) => a.volume - b.volume);
 }
 
-exports.pairStats = (ops) => () => [
+exports.pairStats = ops => () => [
   {query: '/e/allocation/pair/stats'}, 'rout',
-  obj => {
-    return JSON.stringify( sortObj(obj), 0, 2 );
-  }
+  pairs => JSON.stringify(sortPairsByVolume(pairs), 0, 2)
 ];
