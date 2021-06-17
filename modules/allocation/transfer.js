6ba7d637530dae98aab1f4a7418b2df9f0f05395
Agent725 <agent725@725.be>
2021-06-17 16:36:47 +0200

    rename securityDepost and securityWithdraw, add transfer option

---
+++ modules/allocation/transfer.js
@@ -0,0 +1,16 @@
+const {getLogin} = require('../../lib/setup');
+const {getSignatureSteps} = require('./pairDelete');
+
+exports.host = 'allocation';
+exports.args = 3;
+exports.description = 'Transfer an amount from an allocation account [argument: symbol] [argument: amount] [argument: target]';
+
+exports.transfer = (ops) => (symbol, amount, target) => [
+  getLogin(ops, {...this, host: ''}), 'session',
+  target => [
+    ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, -amount]),
+    ({accountID, signature}) => ({query: '/e/allocation/pair/rebalance/' + accountID + '/' + symbol + '/-' + amount + '/' + signature}), 'rout',
+    getLogin(ops, this), 'session', // initialize with allocation session
+    {symbol, amount, target}, 'transaction'
+  ], 'sequential'
+];
