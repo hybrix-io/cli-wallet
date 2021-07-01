93517d605c7bdfda79191ce89bb60575b2a35522
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-07-01 17:39:08 +0200

    Add swap method

---
+++ modules/deal/swap.js
@@ -0,0 +1,21 @@
+const {getLogin} = require('../../lib/setup');
+
+exports.args = 3;
+exports.host = 'allocation';
+exports.description = 'Request and accept a swap deal [argument: base] [argument: symbol] [argument: target_amount]';
+
+exports.swap = ops => (base, symbol, targetAmount) => [
+  getLogin(ops, {...this, host: ''}), 'session',
+  {symbol}, 'getAddress', // get primary allocation address
+  target => ({query: '/e/deal/proposal/' + base + '/' + symbol + '/' + targetAmount + '/' + target}), 'rout',
+  proposal => {
+    const dealID = proposal.id;
+    const amount = proposal.ask.amount;
+    const target = proposal.ask.target;
+    const symbol = proposal.ask.symbol;
+    return [
+      {symbol, amount, target}, 'transaction',
+      transactionId => ({query: '/e/deal/claim/' + transactionId + '/' + dealID}), 'rout'
+    ];
+  }, 'sequential'
+];
