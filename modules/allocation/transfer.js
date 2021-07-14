ee759b60a14d639b34eb03c209b3874b1d5f0529
Agent725 <agent725@725.be>
2021-07-14 14:54:23 +0200

    rename conflicting variables

--- modules/allocation/transfer.js
+++ modules/allocation/transfer.js
@@ -5,12 +5,12 @@ exports.host = 'allocation';
 exports.args = 3;
 exports.description = 'Transfer an amount from an allocation account [argument: symbol] [argument: amount] [argument: target]';
 
-exports.transfer = (ops) => (symbol, amount, target) => [
+exports.transfer = (ops) => (symbol, amount, targetAddress) => [
   getLogin(ops, {...this, host: ''}), 'session',
-  target => [
+  userID => [
     ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, -amount]),
     ({accountID, signature}) => ({query: '/e/allocation/pair/rebalance/' + accountID + '/' + symbol + '/-' + amount + '/' + signature}), 'rout',
     getLogin(ops, this), 'session', // initialize with allocation session
-    {symbol, amount, target}, 'transaction'
+    {symbol, amount, targetAddress}, 'transaction'
   ], 'sequential'
 ];
