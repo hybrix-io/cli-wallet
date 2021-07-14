d2a5bbc803920b133903f7f94f9776979ff1b20f
Agent725 <agent725@725.be>
2021-07-14 22:15:02 +0200

    avoid Javascript math rounding problems to introduce signing errors

--- modules/allocation/transfer.js
+++ modules/allocation/transfer.js
@@ -6,7 +6,7 @@ exports.args = 3;
 exports.description = 'Transfer an amount from an allocation account [argument: symbol] [argument: amount] [argument: target]';
 
 exports.transfer = (ops) => (symbol, amount, target) => [
-  ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, -amount]),
+  ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, '-'+String(amount)]),
   ({accountID, signature}) => ({query: '/e/allocation/pair/rebalance/' + accountID + '/' + symbol + '/-' + amount + '/' + signature}), 'rout',
   getLogin(ops, this), 'session', // initialize with allocation session
   {symbol, amount, target}, 'transaction'
