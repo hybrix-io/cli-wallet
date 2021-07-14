d12b4ccbd72222fe83dfd5be72c411e92d5c221f
Agent725 <agent725@725.be>
2021-07-14 22:04:10 +0200

    optimized transfer module

--- modules/allocation/transfer.js
+++ modules/allocation/transfer.js
@@ -5,12 +5,9 @@ exports.host = 'allocation';
 exports.args = 3;
 exports.description = 'Transfer an amount from an allocation account [argument: symbol] [argument: amount] [argument: target]';
 
-exports.transfer = (ops) => (symbol, amount, targetAddress) => [
-  getLogin(ops, {...this, host: ''}), 'session',
-  userID => [
-    ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, -amount]),
-    ({accountID, signature}) => ({query: '/e/allocation/pair/rebalance/' + accountID + '/' + symbol + '/-' + amount + '/' + signature}), 'rout',
-    getLogin(ops, this), 'session', // initialize with allocation session
-    {symbol, amount, targetAddress}, 'transaction'
-  ], 'sequential'
+exports.transfer = (ops) => (symbol, amount, target) => [
+  ...getSignatureSteps(ops, this, 'rebalancePair', [symbol, -amount]),
+  ({accountID, signature}) => ({query: '/e/allocation/pair/rebalance/' + accountID + '/' + symbol + '/-' + amount + '/' + signature}), 'rout',
+  getLogin(ops, this), 'session', // initialize with allocation session
+  {symbol, amount, target}, 'transaction'
 ];
